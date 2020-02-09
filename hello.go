ee9c1a04f04868d4988db7ec0450fefcb02aac5d
Vitomed <viktor.medyankin@mail.ru>
2020-02-09 18:38:50 +0300

    add reverse

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Vitomed/hello/morestrings"
+)
 
 func main() {
-    fmt.Printf("Hello, Viktor \n")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
